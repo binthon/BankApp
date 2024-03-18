38b3f9523d99f40b1dbd3d8de298c89028f49952
binthon <jakubmedyn@wp.pl>
2024-03-18 19:42:29 +0100

    działający json

--- BankApp.cpp
+++ BankApp.cpp
@@ -1,8 +1,11 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <string>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
 using namespace std;
+using json = nlohmann::json;
 int main() {
     sf::RenderWindow window(sf::VideoMode(400, 200), "Formularz SFML");
 
@@ -41,10 +44,10 @@ int main() {
     submitText.setPosition(15.f, 100.f);
     submitText.setFillColor(sf::Color::White);
 
-    bool isTextField1Active = false;
-    bool isTextField2Active = false;
     std::string inputText1 = "";
     std::string inputText2 = "";
+    bool isTextField1Active = false;
+    bool isTextField2Active = false;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -54,7 +57,18 @@ int main() {
             }
 
             if (event.type == sf::Event::MouseButtonPressed) {
-                if (textField1.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                if (submitButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                    json j;
+                    j["login"] = inputText1;
+                    j["password"] = inputText2;
+
+                    ofstream outputFile("loginData.json");
+                    outputFile << j.dump(4);
+                    outputFile.close();
+
+                    cout << "Data saved to loginData.json" << endl;
+                }
+                else if (textField1.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
                     isTextField1Active = true;
                     isTextField2Active = false;
                     textField1.setOutlineColor(sf::Color::Blue);
@@ -66,9 +80,6 @@ int main() {
                     textField1.setOutlineColor(sf::Color::Black);
                     textField2.setOutlineColor(sf::Color::Blue);
                 }
-                else if (submitButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
-                    cout << "Submit";
-                }
                 else {
                     isTextField1Active = false;
                     isTextField2Active = false;
@@ -79,18 +90,18 @@ int main() {
 
             if (event.type == sf::Event::TextEntered) {
                 if (isTextField1Active) {
-                    if (event.text.unicode == '\b' && !inputText1.empty()) { // Backspace
+                    if (event.text.unicode == '\b' && !inputText1.empty()) { 
                         inputText1.pop_back();
                     }
-                    else if (event.text.unicode < 128 && event.text.unicode != '\b') { // Normal character
+                    else if (event.text.unicode < 128 && event.text.unicode != '\b') { 
                         inputText1 += static_cast<char>(event.text.unicode);
                     }
                 }
                 else if (isTextField2Active) {
-                    if (event.text.unicode == '\b' && !inputText2.empty()) { // Backspace
+                    if (event.text.unicode == '\b' && !inputText2.empty()) { 
                         inputText2.pop_back();
                     }
-                    else if (event.text.unicode < 128 && event.text.unicode != '\b') { // Normal character
+                    else if (event.text.unicode < 128 && event.text.unicode != '\b') { 
                         inputText2 += static_cast<char>(event.text.unicode);
                     }
                 }
